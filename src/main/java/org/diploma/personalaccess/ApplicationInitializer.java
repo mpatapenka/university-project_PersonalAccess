bbe9fe76eff754b0995e74288dfcc531484d7e6c
trollsrulls <maxim131994@gmail.com>
2016-01-25 19:49:54 +0300

    Initial commit

---
+++ src/main/java/org/diploma/personalaccess/ApplicationInitializer.java
@@ -0,0 +1,29 @@
+package org.diploma.personalaccess;
+
+import org.diploma.personalaccess.config.WebConfig;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+public class ApplicationInitializer implements WebApplicationInitializer {
+
+    private static final String DISPATCHER = "dispatcher";
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+        context.register(WebConfig.class);
+
+        servletContext.addListener(new ContextLoaderListener(context));
+
+        ServletRegistration.Dynamic dispatcher = servletContext.addServlet(DISPATCHER, new DispatcherServlet(context));
+        dispatcher.addMapping("/");
+        dispatcher.setLoadOnStartup(1);
+    }
+
+}
---
+++ src/main/java/org/diploma/personalaccess/controller/IndexController.java
@@ -0,0 +1,18 @@
+package org.diploma.personalaccess.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/pa")
+public class IndexController {
+
+    @RequestMapping(value = "/get", method = RequestMethod.GET)
+    @ResponseBody
+    public String getTest() {
+        return "Hello, World!";
+    }
+
+}

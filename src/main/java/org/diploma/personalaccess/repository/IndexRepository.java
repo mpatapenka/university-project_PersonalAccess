fd18a8cc094165f1f17a639d76e21feefa8131b1
trollsrulls <maxim131994@gmail.com>
2016-02-19 02:07:05 +0300

    Fix repository retrieve method name

--- src/main/java/org/diploma/personalaccess/repository/IndexRepository.java
+++ src/main/java/org/diploma/personalaccess/repository/IndexRepository.java
@@ -1,12 +1,13 @@
 package org.diploma.personalaccess.repository;
 
 import org.diploma.personalaccess.entity.Index;
+import org.diploma.personalaccess.entity.Position;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface IndexRepository extends JpaRepository<Index, Long> {
 
-    List<Index> findIndexesByPositionId(Long positionId);
+    List<Index> findIndexesByAvailablePositions(Position position);
 
 }
--- src/main/java/org/diploma/personalaccess/service/PersonalAccessServiceImpl.java
+++ src/main/java/org/diploma/personalaccess/service/PersonalAccessServiceImpl.java
@@ -18,7 +18,7 @@ public class PersonalAccessServiceImpl implements PersonalAccessService {
     @Override
     @Transactional
     public List<Index> findIndexesByPosition(final Position position) {
-        return indexRepository.findIndexesByPositionId(position.getId());
+        return indexRepository.findIndexesByAvailablePositions(position);
     }
 
     @Override

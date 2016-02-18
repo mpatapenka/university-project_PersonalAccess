1ceb8da28417f1b60655f9890b6d5b77a276bbe7
Maksim Patapenka <maksim.patapenka@datalex.com>
2016-02-18 15:53:53 +0300

    Added jpa repository for JavaBean Index

---
+++ src/main/java/org/diploma/personalaccess/repository/IndexRepository.java
@@ -0,0 +1,8 @@
+package org.diploma.personalaccess.repository;
+
+import org.diploma.personalaccess.entity.Index;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IndexRepository extends JpaRepository<Index, Long> {
+
+}

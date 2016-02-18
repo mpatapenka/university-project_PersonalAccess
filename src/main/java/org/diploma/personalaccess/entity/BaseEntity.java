96dab467fd8d5c90046985d62ace9ca14148f272
Maksim Patapenka <maksim.patapenka@datalex.com>
2016-02-18 16:43:18 +0300

    Changed type of ID from long to Long, for checking new objects

--- src/main/java/org/diploma/personalaccess/entity/BaseEntity.java
+++ src/main/java/org/diploma/personalaccess/entity/BaseEntity.java
@@ -18,14 +18,18 @@ public class BaseEntity implements Serializable {
     @Id
     @GeneratedValue(generator = "increment")
     @GenericGenerator(name = "increment", strategy = "increment")
-    protected long id;
+    protected Long id;
 
-    public long getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
+    }
+
+    public boolean isNew() {
+        return (this.id == null);
     }
 
 }

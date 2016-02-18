33f3f14846128fba132610931864044e5c366e99
Maksim Patapenka <maksim.patapenka@datalex.com>
2016-02-18 16:43:49 +0300

    Added search by criteria method in IndexRepository

--- src/main/java/org/diploma/personalaccess/repository/IndexRepository.java
+++ src/main/java/org/diploma/personalaccess/repository/IndexRepository.java
@@ -3,6 +3,10 @@ package org.diploma.personalaccess.repository;
 import org.diploma.personalaccess.entity.Index;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface IndexRepository extends JpaRepository<Index, Long> {
+
+    List<Index> findIndexesByPositionId(Long positionId);
 
 }

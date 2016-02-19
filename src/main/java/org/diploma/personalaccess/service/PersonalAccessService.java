f14dda47c5db10b6fc3e83925dc5020b28f4b924
Maksim Patapenka <maksim.patapenka@datalex.com>
2016-02-19 13:01:26 +0300

    Updated PA service and implementation

--- src/main/java/org/diploma/personalaccess/service/PersonalAccessService.java
+++ src/main/java/org/diploma/personalaccess/service/PersonalAccessService.java
@@ -2,7 +2,9 @@ package org.diploma.personalaccess.service;
 
 import org.diploma.personalaccess.entity.Index;
 import org.diploma.personalaccess.entity.Position;
+import org.diploma.personalaccess.entity.UserIndex;
 
+import java.sql.Date;
 import java.util.List;
 
 public interface PersonalAccessService {
@@ -12,5 +14,13 @@ public interface PersonalAccessService {
     void saveOrUpdateIndex(Index index);
 
     void deleteIndex(Index index);
+
+
+
+    List<UserIndex> findUserIndexesByPositionAndPeriod(Position position, Date startPeriod, Date endPeriod);
+
+    void saveUserIndexes(List<UserIndex> userIndexes);
+
+    void setLeadEstimateUserIndexes(List<UserIndex> rawUserIndexes);
 
 }
--- src/main/java/org/diploma/personalaccess/service/PersonalAccessServiceImpl.java
+++ src/main/java/org/diploma/personalaccess/service/PersonalAccessServiceImpl.java
@@ -2,11 +2,14 @@ package org.diploma.personalaccess.service;
 
 import org.diploma.personalaccess.entity.Index;
 import org.diploma.personalaccess.entity.Position;
+import org.diploma.personalaccess.entity.UserIndex;
 import org.diploma.personalaccess.repository.IndexRepository;
+import org.diploma.personalaccess.repository.UserIndexRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
+import java.sql.Date;
 import java.util.List;
 
 @Service
@@ -14,6 +17,11 @@ public class PersonalAccessServiceImpl implements PersonalAccessService {
 
     @Resource
     private IndexRepository indexRepository;
+
+    @Resource
+    private UserIndexRepository userIndexRepository;
+
+
 
     @Override
     @Transactional
@@ -40,6 +48,33 @@ public class PersonalAccessServiceImpl implements PersonalAccessService {
     @Transactional
     public void deleteIndex(final Index index) {
         indexRepository.delete(index);
+    }
+
+
+
+    @Override
+    @Transactional
+    public List<UserIndex> findUserIndexesByPositionAndPeriod(final Position position, final Date startPeriod, final Date endPeriod) {
+        return userIndexRepository.findByPositionAndFillDateBetweenOrderByIndexId(position, startPeriod, endPeriod);
+    }
+
+    @Override
+    @Transactional
+    public void saveUserIndexes(final List<UserIndex> userIndexes) {
+        for (UserIndex userIndex : userIndexes) {
+            userIndexRepository.save(userIndex);
+        }
+    }
+
+    @Override
+    @Transactional
+    public void setLeadEstimateUserIndexes(final List<UserIndex> rawUserIndexes) {
+        for (UserIndex rawUserIndex : rawUserIndexes) {
+            UserIndex userIndex = userIndexRepository.findOne(rawUserIndex.getId());
+            userIndex.setLeadEstimate(rawUserIndex.getLeadEstimate());
+
+            userIndexRepository.save(userIndex);
+        }
     }
 
 }

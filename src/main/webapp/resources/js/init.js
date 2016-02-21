9ec50a2788fbd194d9a0ff8e46803deb5fa7c57a
trollsrulls <maxim131994@gmail.com>
2016-02-21 18:23:17 +0300

    Added custom front-end files

---
+++ src/main/webapp/resources/js/init.js
@@ -0,0 +1,8 @@
+// Instantiate side navigation bar
+;(function ($) {
+    $(function () {
+
+        $('.button-collapse').sideNav();
+
+    }); // end of document ready
+})(jQuery); // end of jQuery name space

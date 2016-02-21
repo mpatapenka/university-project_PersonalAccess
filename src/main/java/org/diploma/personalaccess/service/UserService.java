8d72eca9ce051f2a146b06811b3fbc9cbdccf521
trollsrulls <maxim131994@gmail.com>
2016-02-22 00:39:26 +0300

    Added user repo and service implementation

---
+++ src/main/java/org/diploma/personalaccess/service/UserService.java
@@ -0,0 +1,28 @@
+package org.diploma.personalaccess.service;
+
+import org.diploma.personalaccess.entity.User;
+import org.diploma.personalaccess.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+public class UserService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User with such credentials was not found!");
+        }
+        return user;
+    }
+
+}

7fb95f627c6ccc712445879f4c75f3649faf0d60
trollsrulls <maxim131994@gmail.com>
2016-02-21 16:03:33 +0300

    Fixed database config

--- src/main/java/org/diploma/personalaccess/config/DatabaseConfig.java
+++ src/main/java/org/diploma/personalaccess/config/DatabaseConfig.java
@@ -63,11 +63,11 @@ public class DatabaseConfig {
     }
 
     @Bean
-    public PlatformTransactionManager platformTransactionManager() {
+    public PlatformTransactionManager transactionManager() {
         JpaTransactionManager manager = new JpaTransactionManager();
         manager.setEntityManagerFactory(entityManagerFactory().getObject());
 
-        return  manager;
+        return manager;
     }
 
     private Properties getHibernateProperties() {

b274a968609ae21d4b0f8e83d31300eee48434cd
trollsrulls <maxim131994@gmail.com>
2016-02-22 00:38:43 +0300

    Added instantiation character counter for forms

--- src/main/webapp/resources/js/init.js
+++ src/main/webapp/resources/js/init.js
@@ -6,3 +6,8 @@
 
     }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+// Instantiate character counter
+$(document).ready(function () {
+    $('input#input_text, textarea#textarea1').characterCounter();
+});

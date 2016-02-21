84d063886d251b9057491020c383722b05638fad
trollsrulls <maxim131994@gmail.com>
2016-02-22 00:39:05 +0300

    Added security configuration

---
+++ src/main/java/org/diploma/personalaccess/SecurityInitializer.java
@@ -0,0 +1,10 @@
+package org.diploma.personalaccess;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+/**
+ * Empty class for loading security filter into context
+ */
+public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}

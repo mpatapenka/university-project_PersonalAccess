fa547cd94758abae4dc9ed21f21c6d338a405cca
trollsrulls <maxim131994@gmail.com>
2016-02-21 18:26:14 +0300

    Added service to controller

--- src/main/java/org/diploma/personalaccess/controller/IndexController.java
+++ src/main/java/org/diploma/personalaccess/controller/IndexController.java
@@ -1,11 +1,16 @@
 package org.diploma.personalaccess.controller;
 
+import org.diploma.personalaccess.service.PersonalAccessService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class IndexController {
+
+    @Autowired
+    private PersonalAccessService personalAccessService;
 
     @RequestMapping(value = {"/", "/dashboard"}, method = RequestMethod.GET)
     public String getDashboardPage() {

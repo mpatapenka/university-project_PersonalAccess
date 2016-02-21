df7f61db040e9e3148aa66450dabc419507fab6c
trollsrulls <maxim131994@gmail.com>
2016-02-21 18:25:01 +0300

    Fixed missing repo annotations

--- src/main/java/org/diploma/personalaccess/repository/IndexRepository.java
+++ src/main/java/org/diploma/personalaccess/repository/IndexRepository.java
@@ -3,9 +3,11 @@ package org.diploma.personalaccess.repository;
 import org.diploma.personalaccess.entity.Index;
 import org.diploma.personalaccess.entity.Position;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface IndexRepository extends JpaRepository<Index, Long> {
 
     List<Index> findIndexesByAvailablePositions(Position position);
--- src/main/java/org/diploma/personalaccess/repository/UserIndexRepository.java
+++ src/main/java/org/diploma/personalaccess/repository/UserIndexRepository.java
@@ -3,12 +3,14 @@ package org.diploma.personalaccess.repository;
 import org.diploma.personalaccess.entity.Position;
 import org.diploma.personalaccess.entity.UserIndex;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.sql.Date;
 import java.util.List;
 
+@Repository
 public interface UserIndexRepository extends JpaRepository<UserIndex, Long> {
 
-    List<UserIndex> findByPositionAndFillDateBetweenOrderByIndexId(Position position, Date startPeriod, Date endPeriod);
+    List<UserIndex> findByUserFormPositionAndFillDateBetweenOrderByIndexId(Position position, Date startPeriod, Date endPeriod);
 
 }

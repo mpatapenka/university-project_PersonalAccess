5ae6e863f0461cce020f788651c180ca97c85b8e
Maksim Patapenka <maksim.patapenka@datalex.com>
2016-02-22 15:02:50 +0300

    Added document repository

---
+++ src/main/java/org/diploma/personalaccess/repository/DocumentRepository.java
@@ -0,0 +1,10 @@
+package org.diploma.personalaccess.repository;
+
+import org.diploma.personalaccess.entity.Document;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DocumentRepository extends JpaRepository<Document, Long> {
+
+}

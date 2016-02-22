6d285a9f5f5db3ff39d75282cbf3110e6d126670
trollsrulls <maxim131994@gmail.com>
2016-02-22 22:35:22 +0300

    Updated security config

--- src/main/java/org/diploma/personalaccess/config/SecurityConfig.java
+++ src/main/java/org/diploma/personalaccess/config/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .exceptionHandling()
-                .accessDeniedPage("/403")
+                .accessDeniedPage("/error/403")
             .and()
 
             .authorizeRequests()

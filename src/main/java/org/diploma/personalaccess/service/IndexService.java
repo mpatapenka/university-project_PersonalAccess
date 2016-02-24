22e4d4ebac9ffa6dc1e1317eb31f4b81f4fd0e2c
trollsrulls <maxim131994@gmail.com>
2016-02-25 00:40:30 +0300

    Added index service and impl

---
+++ src/main/java/org/diploma/personalaccess/service/IndexService.java
@@ -0,0 +1,9 @@
+package org.diploma.personalaccess.service;
+
+import org.diploma.personalaccess.entity.Index;
+
+public interface IndexService {
+
+    void saveOrUpdateIndex(Index index);
+
+}
---
+++ src/main/java/org/diploma/personalaccess/service/impl/IndexServiceImpl.java
@@ -0,0 +1,29 @@
+package org.diploma.personalaccess.service.impl;
+
+import org.diploma.personalaccess.entity.Index;
+import org.diploma.personalaccess.repository.IndexRepository;
+import org.diploma.personalaccess.service.IndexService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class IndexServiceImpl implements IndexService {
+
+    @Autowired
+    private IndexRepository indexRepository;
+
+    @Override
+    @Transactional
+    public void saveOrUpdateIndex(final Index index) {
+        Index updated = index.isNew() ? new Index() : indexRepository.findOne(index.getId());
+        updated.setEstimate(index.getEstimate());
+        updated.setMultiplier(index.getMultiplier());
+        updated.setName(index.getName());
+        updated.setWorkName(index.getWorkName());
+        updated.setAvailablePositions(index.getAvailablePositions());
+
+        indexRepository.save(updated);
+    }
+
+}

722017c64692326869fcb97206e448563750f453
Maksim Patapenka <maksim.patapenka@datalex.com>
2016-02-25 12:37:00 +0300

    Added comments to used repositories

--- src/main/java/org/diploma/personalaccess/repository/PositionRepository.java
+++ src/main/java/org/diploma/personalaccess/repository/PositionRepository.java
@@ -4,7 +4,11 @@ import org.diploma.personalaccess.entity.Position;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Simple realization JPA repository for JavaBean Position
+ *
+ * @author Maksim Patapenka
+ */
 @Repository
 public interface PositionRepository extends JpaRepository<Position, Long> {
-
 }
--- src/main/java/org/diploma/personalaccess/repository/UserRepository.java
+++ src/main/java/org/diploma/personalaccess/repository/UserRepository.java
@@ -4,9 +4,20 @@ import org.diploma.personalaccess.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Simple realization JPA repository for JavaBean User
+ *
+ * @author Maksim Patapenka
+ */
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    /**
+     * Retrieve 'user' from database by his 'username'
+     *
+     * @param username of user
+     * @return user from database
+     */
     User findByUsername(String username);
 
 }

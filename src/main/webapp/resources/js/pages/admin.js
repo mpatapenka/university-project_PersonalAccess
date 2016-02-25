1e1f143cfb106f976bbdd32d71a500b77f60e59b
Maksim Patapenka <maksim.patapenka@datalex.com>
2016-02-25 12:31:09 +0300

    Updated method for creating json object from form and send/receive it to rest controller

--- src/main/webapp/resources/js/pages/admin.js
+++ src/main/webapp/resources/js/pages/admin.js
@@ -3,20 +3,31 @@
 
 function addNewIndex() {
     var form = $("#add-index-form");
-    var data = form.serialize();
     var selectedPoses = $("#selected-poses").val();
+
+    var poses = [];
+    $.each(selectedPoses, function(index, value) {
+        poses.push({
+            id: value,
+            name: ""
+        });
+    });
+
+    var index = form.serializeObject();
+    index.availablePositions = poses;
+
+    var jsonIndex = JSON.stringify(index);
 
     $.ajax({
         url: form.attr("action"),
         type: form.attr("method"),
         dataType: "json",
-        data: decodeURI(data),
+        data: jsonIndex,
         success: function (result) {
             alert("success: " + result);
         },
         error: function (error) {
             alert("error: " + error);
-            //location.reload();
         }
     });
 }

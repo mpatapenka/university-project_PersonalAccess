b57c9ddd227388aef6daf40e33dc04c4717af0bb
trollsrulls <maxim131994@gmail.com>
2016-02-26 23:32:47 +0300

    Implemented period holder

--- src/main/java/org/diploma/personalaccess/service/holder/impl/PeriodHolderImpl.java
+++ src/main/java/org/diploma/personalaccess/service/holder/impl/PeriodHolderImpl.java
@@ -3,6 +3,8 @@ package org.diploma.personalaccess.service.holder.impl;
 import org.apache.log4j.Logger;
 import org.diploma.personalaccess.bean.Period;
 import org.diploma.personalaccess.service.holder.PeriodHolder;
+import org.diploma.personalaccess.util.Parser;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -14,6 +16,7 @@ import java.util.*;
  *
  * @author Maksim Patapenka
  */
+@Component
 public class PeriodHolderImpl implements PeriodHolder {
 
     /**
@@ -22,14 +25,33 @@ public class PeriodHolderImpl implements PeriodHolder {
     private static final String HOLDER_CONFIG_FILE = "periods.properties";
 
     /**
+     * Default period name code. It's a quarter
+     */
+    private static final String DEFAULT_PERIOD_NAME_CODE = "QUARTER";
+
+    /**
+     * Default period count. For quarters it's '4'
+     */
+    private static final String DEFAULT_PERIOD_COUNT = "4";
+
+    /**
      * Logger Log4j
      */
     private static final Logger log = Logger.getLogger(PeriodHolderImpl.class);
+
+
 
     /**
      * List which contains all period. In memory holder
      */
     private List<Period> entries;
+
+    /**
+     * Period name code. Code using for index for translation on page
+     */
+    private String periodNameCode;
+
+
 
     public PeriodHolderImpl() {
         entries = new ArrayList<>();
@@ -39,33 +61,74 @@ public class PeriodHolderImpl implements PeriodHolder {
             Properties properties = new Properties();
             properties.load(is);
 
+
+            periodNameCode = properties.getProperty("code", DEFAULT_PERIOD_NAME_CODE);
+            final int periodsCount = Integer.parseInt(properties.getProperty("count", DEFAULT_PERIOD_COUNT));
+            for (int i = 0; i < periodsCount; i++) {
+                String key = createPeriodKey(i);
+                String periodData = properties.getProperty(key);
+                Period period = Parser.convertPeriodDataStringToPeriod(periodData);
+                entries.add(period);
+            }
+
+            Collections.sort(entries);
+
         } catch (IOException e) {
             String msg = "Can not find property file '" + HOLDER_CONFIG_FILE + "' in classpath.";
+            log.error(msg, e);
+            throw new IllegalArgumentException(msg, e);
+        } catch (NumberFormatException e) {
+            String msg = "Check property config file. Any fields have incorrect values.";
             log.error(msg, e);
             throw new IllegalArgumentException(msg, e);
         }
     }
 
+    private static String createPeriodKey(int index) {
+        /* Default key prefix */
+        final String keyPrefix = "p";
+        return keyPrefix + index;
+    }
+
+
+
     @Override
     public Period getPeriodByDate(Date date) {
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(date);
+        int year = calendar.get(Calendar.YEAR);
 
-        int cDay = calendar.get(Calendar.DAY_OF_MONTH);
-        int cMonth = calendar.get(Calendar.MONTH);
+        for (Period period : entries) {
+            Date start = period.getStartDateForYear(year);
+            Date end = period.getEndDateForYear(year);
 
-        for (Period entry : entries) {
-
+            if (date.compareTo(start) >= 0 /* date after or equal start date */
+                    && date.compareTo(end) <= 0 /* date before or equal end date */) {
+                return period;
+            }
         }
 
-        String msg = "Period for date '" + date + "' not found. Check your configuration.";
+        String msg = "No one period found for today '" + date
+                + "', please reconfigure PeriodHolder so all days should be in any period.";
         log.error(msg);
         throw new IllegalArgumentException(msg);
     }
 
     @Override
+    public Period getCurrentPeriod() {
+        Calendar calendar = Calendar.getInstance();
+        Date today = new Date(calendar.getTime().getTime());
+        return getPeriodByDate(today);
+    }
+
+    @Override
     public List<Period> getAllPeriods() {
         return entries;
+    }
+
+    @Override
+    public String getPeriodsNameCode() {
+        return periodNameCode;
     }
 
 }

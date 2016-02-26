777c751ef1f8b5201ecbfc024240a8b4a43c022b
trollsrulls <maxim131994@gmail.com>
2016-02-27 02:32:15 +0300

    Updated test, refactoried

--- src/test/java/org/diploma/personalaccess/repository/PersonalAccessPersistenceTest.java
+++ src/test/java/org/diploma/personalaccess/repository/PersonalAccessPersistenceTest.java
@@ -1,27 +1,24 @@
-package org.diploma.personalaccess.service;
+package org.diploma.personalaccess.repository;
 
 import org.diploma.personalaccess.config.DatabaseConfig;
 import org.diploma.personalaccess.entity.Index;
-import org.diploma.personalaccess.repository.IndexRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 import static org.junit.Assert.*;
 
 /**
- * Tests for index repository
+ * Tests for repository
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = DatabaseConfig.class)
 @WebAppConfiguration
-@Transactional
 public class PersonalAccessPersistenceTest {
 
     /**
@@ -31,7 +28,7 @@ public class PersonalAccessPersistenceTest {
     private IndexRepository indexRepository;
 
     /**
-     * Complex test
+     * Complex index test
      */
     @Test
     public void testThatIndexCanBeSaveAndGetWithActualValues() {

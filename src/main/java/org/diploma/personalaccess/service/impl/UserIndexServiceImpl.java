5ab233ca034e90292225b57c217162fd7232d8b0
trollsrulls <maxim131994@gmail.com>
2016-02-28 13:14:29 +0300

    Created mock implementation if user index service

--- src/main/java/org/diploma/personalaccess/service/impl/UserIndexServiceImpl.java
+++ src/main/java/org/diploma/personalaccess/service/impl/UserIndexServiceImpl.java
@@ -1,5 +1,6 @@
 package org.diploma.personalaccess.service.impl;
 
+import org.apache.commons.codec.digest.DigestUtils;
 import org.diploma.personalaccess.bean.Period;
 import org.diploma.personalaccess.entity.User;
 import org.diploma.personalaccess.entity.UserIndex;
@@ -7,7 +8,11 @@ import org.diploma.personalaccess.repository.UserIndexRepository;
 import org.diploma.personalaccess.service.UserIndexService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 /**
@@ -28,7 +33,49 @@ public class UserIndexServiceImpl implements UserIndexService {
 
     @Override
     public List<UserIndex> getAllUserIndexesByCurrentPeriod(User user, Period period) {
-        return null;
+        // TODO: implement this
+        return new ArrayList<>();
+    }
+
+    @Override
+    public boolean isUserIndexesAvailableForPeriod(User user, Period period) {
+        // TODO: implement this
+        return false;
+    }
+
+    @Override
+    @Transactional
+    public void publishAllUserIndexes(List<UserIndex> userIndexes, User user) {
+        // TODO: implement support of document uploading
+
+        Calendar calendar = Calendar.getInstance();
+        Date date = new Date(calendar.getTime().getTime());
+        for (UserIndex userIndex : userIndexes) {
+            String docName = userIndex.getDocument().getName();
+            if (!docName.isEmpty()) {
+                userIndex.getDocument().setSystemName(generateUniqueSystemName(docName));
+            } else {
+                userIndex.setDocument(null);
+            }
+
+            userIndex.setUser(user);
+            userIndex.setFillDate(date);
+
+            userIndexRepository.save(userIndex);
+        }
+    }
+
+
+
+    /**
+     * Generate unique file name, which depend on current
+     * time in milliseconds
+     *
+     * @param name original filename
+     * @return unique file name for file system
+     */
+    private static String generateUniqueSystemName(String name) {
+        return DigestUtils.sha256Hex(name) + System.currentTimeMillis();
     }
 
 }

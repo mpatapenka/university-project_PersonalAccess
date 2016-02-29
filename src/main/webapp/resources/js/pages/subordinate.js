fd70515a210a4695f79941725b4a9fb509a585dc
Maksim Patapenka <maksim.patapenka@datalex.com>
2016-02-29 17:44:53 +0300

    Updated subordinate scripts

--- src/main/webapp/resources/js/pages/subordinate.js
+++ src/main/webapp/resources/js/pages/subordinate.js
@@ -25,28 +25,23 @@ function search() {
 
             $("#card-container").empty();
 
-            var indexes = JSON.parse(result);
+            var parsedResult = JSON.parse(result);
+            var isSubmitted = parsedResult.isSubmitted;
+            var indexes = parsedResult.subIndexes;
             var content = "<ul class='collection'>";
             for (var i = 0; i < indexes.length; i++) {
-                var card = createItemContext(indexes[i]);
-                content += card;
+                content += isSubmitted ? createSubmittedItemContext : createItemContext(indexes[i]);
             }
             content += "</ul>"
 
             $("#card-container").append(content);
 
             var btnBox = $("#float-box");
-            btnBox.addClass("click-to-toggle");
-            btnBox.empty();
-
-            var btnContent = "<a class='btn-floating btn-large red'><i class='large mdi-navigation-menu'></i></a>";
-            btnContent += "<ul><li>";
-            btnContent += "<a class='btn-floating cyan darken-3' id='search-btn'><i class='material-icons'>youtube_searched_for</i></a>";
-            btnContent += "</li><li>";
-            btnContent += "<a class='btn-floating green' id='publish-btn'><i class='material-icons'>publish</i></a>";
-            btnContent += "</li></ul>";
-
-            btnBox.append(btnContent);
+            if (isSubmitted) {
+                createFloatBtnBox(btnBox);
+            } else {
+                createFloatBtnBoxWithButtons(btnBox);
+            }
         },
         error: function (error) {
             console.log("error: " + error);
@@ -73,7 +68,23 @@ function publish() {
         return;
     }
 
+    $.ajax({
+        url: "/user/subs?publish",
+        type: "post",
+        data: leadEstimates,
+        success: function (result) {
+            console.log("success: " + result);
 
+            if (result === "success") {
+                location.reload();
+            }
+            Materialize.toast("Во время обновления произошла ошибка", 4000);
+        },
+        error: function (error) {
+            console.log("error: " + error);
+            Materialize.toast("Во время поиска произошла ошибка", 4000);
+        }
+    });
 }
 
 
@@ -106,4 +117,59 @@ function createItemContext(index) {
     card += "</div>";
 
     return card;
+}
+
+function createSubmittedItemContext(index) {
+    var card = "<li class='collection-item avatar'><i class='material-icons circle green'>"
+        + "insert_chart</i><span class='title truncate'>";
+    card += index.index.name;
+    card += "</span>";
+    card += "<p>Оценка подчиненного: " + index.selfEstimate + "<br>";
+    card += "Дата заполнения: " + index.fillDate + "</p>";
+    card += "<p>Ваша оценка:<br>" + index.leadEstimate + "</p>"
+    card += "<a href='JavaScript:$(\"#additional-modal-" + index.id + "\").openModal();' class='secondary-content'>"
+        + "<i class='material-icons'>chat_bubble_outline</i></a>";
+    card += "<div id='additional-modal-"+ index.id +"' class='modal bottom-sheet'>";
+    card += "<div class='modal-content'><h4>Дополнительная информация</h4><hr>";
+    card += "<h5>" + index.index.name + "</h5><br>";
+    card += "<p>Максимальная оценка " + index.index.estimate + " за каждые " + index.index.multiplier + " " + index.index.workName + "</p>";
+
+    if (index.description !== null && index.description !== undefined) {
+        card += "<br><p>Описание проделанной работы: " + index.description + "</p>";
+    }
+
+    if (index.document !== null && index.document !== undefined) {
+        card += "<br><p>Скачать прикрепленный документ: <a href='#'>" + index.document.name + "</a></p>";
+    }
+
+    card += "</div>";
+
+    card += "<div class='modal-footer'><a class='modal-action modal-close waves-effect waves-red btn-flat'>Закрыть</a>";
+    card += "</div>";
+
+    return card;
+}
+
+function createFloatBtnBoxWithButtons(box) {
+    box.addClass("click-to-toggle");
+    box.empty();
+
+    var btnContent = "<a class='btn-floating btn-large red'><i class='large mdi-navigation-menu'></i></a>";
+    btnContent += "<ul><li>";
+    btnContent += "<a class='btn-floating cyan darken-3' id='search-btn'><i class='material-icons'>youtube_searched_for</i></a>";
+    btnContent += "</li><li>";
+    btnContent += "<a class='btn-floating green' id='publish-btn'><i class='material-icons'>publish</i></a>";
+    btnContent += "</li></ul>";
+
+    box.append(btnContent);
+}
+
+function createFloatBtnBox(box) {
+    box.removeClass("click-to-toggle");
+    box.empty();
+
+    var btnContent = "<a class='btn-floating btn-large waves-effect waves-light cyan darken-3' id='search-btn'>"
+        + "<i class='material-icons'>youtube_searched_for</i></a>";
+
+    btnBox.append(btnContent);
 }

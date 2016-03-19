e0c8f3b1f112dc830f7b6dd304acce0686019ef0
trollsrulls <maxim131994@gmail.com>
2016-03-19 22:12:22 +0300

    Added number validator

--- src/main/webapp/resources/js/scripts.js
+++ src/main/webapp/resources/js/scripts.js
@@ -12,6 +12,21 @@ function emptyValidator() {
     return isValid;
 }
 
+// Validate fields with type 'number' for not negative values
+function numberBiggerThenZeroValidator() {
+    var isValid = true;
+    $("input[type='number']").each(function () {
+        var value = $(this);
+        value.removeClass("invalid");
+        if (parseFloat(value.val().trim()) < 0) {
+            value.addClass("invalid");
+            isValid = false;
+        }
+    });
+    return isValid;
+}
+
+
 // Converting JS object to json string with replaced '%' by '%25'
 // It need for transfer by http protocol
 function convertToJsonForTransfer(obj) {

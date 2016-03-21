aac7b9c8eaf9ebfb653c87252f3b2f62f362e93d
trollsrulls <maxim131994@gmail.com>
2016-03-22 00:28:28 +0300

    Refactoried position service

--- src/main/java/org/diploma/personalaccess/service/impl/PositionServiceImpl.java
+++ src/main/java/org/diploma/personalaccess/service/impl/PositionServiceImpl.java
@@ -24,7 +24,6 @@ public class PositionServiceImpl implements PositionService {
     private PositionRepository positionRepository;
 
 
-
     @Override
     @Transactional
     public List<Position> findAll() {

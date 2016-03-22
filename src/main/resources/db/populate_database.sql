52eae527f7d0ac317350f52288013828e2107926
trollsrulls <maxim131994@gmail.com>
2016-03-22 22:29:59 +0300

    Add mapping from index to position (db data)

--- src/main/resources/db/populate_database.sql
+++ src/main/resources/db/populate_database.sql
@@ -274,3 +274,40 @@ INSERT INTO `personal_access_database`.`_index` (`name`, `estimate`, `multiplier
   '1',
   ''
 );
+
+-- Map dean indexes to position
+INSERT INTO `personal_access_database`.`available_index` (`index_id`, `position_id`) VALUES ('1', '3');
+INSERT INTO `personal_access_database`.`available_index` (`index_id`, `position_id`) VALUES ('2', '3');
+INSERT INTO `personal_access_database`.`available_index` (`index_id`, `position_id`) VALUES ('3', '3');
+INSERT INTO `personal_access_database`.`available_index` (`index_id`, `position_id`) VALUES ('4', '3');
+INSERT INTO `personal_access_database`.`available_index` (`index_id`, `position_id`) VALUES ('5', '3');
+INSERT INTO `personal_access_database`.`available_index` (`index_id`, `position_id`) VALUES ('6', '3');
+INSERT INTO `personal_access_database`.`available_index` (`index_id`, `position_id`) VALUES ('7', '3');
+INSERT INTO `personal_access_database`.`available_index` (`index_id`, `position_id`) VALUES ('8', '3');
+INSERT INTO `personal_access_database`.`available_index` (`index_id`, `position_id`) VALUES ('9', '3');
+INSERT INTO `personal_access_database`.`available_index` (`index_id`, `position_id`) VALUES ('10', '3');
+INSERT INTO `personal_access_database`.`available_index` (`index_id`, `position_id`) VALUES ('11', '3');
+INSERT INTO `personal_access_database`.`available_index` (`index_id`, `position_id`) VALUES ('12', '3');
+INSERT INTO `personal_access_database`.`available_index` (`index_id`, `position_id`) VALUES ('13', '3');
+INSERT INTO `personal_access_database`.`available_index` (`index_id`, `position_id`) VALUES ('14', '3');
+INSERT INTO `personal_access_database`.`available_index` (`index_id`, `position_id`) VALUES ('15', '3');
+INSERT INTO `personal_access_database`.`available_index` (`index_id`, `position_id`) VALUES ('16', '3');
+INSERT INTO `personal_access_database`.`available_index` (`index_id`, `position_id`) VALUES ('17', '3');
+INSERT INTO `personal_access_database`.`available_index` (`index_id`, `position_id`) VALUES ('18', '3');
+INSERT INTO `personal_access_database`.`available_index` (`index_id`, `position_id`) VALUES ('19', '3');
+INSERT INTO `personal_access_database`.`available_index` (`index_id`, `position_id`) VALUES ('20', '3');
+INSERT INTO `personal_access_database`.`available_index` (`index_id`, `position_id`) VALUES ('21', '3');
+INSERT INTO `personal_access_database`.`available_index` (`index_id`, `position_id`) VALUES ('22', '3');
+INSERT INTO `personal_access_database`.`available_index` (`index_id`, `position_id`) VALUES ('23', '3');
+INSERT INTO `personal_access_database`.`available_index` (`index_id`, `position_id`) VALUES ('24', '3');
+INSERT INTO `personal_access_database`.`available_index` (`index_id`, `position_id`) VALUES ('25', '3');
+INSERT INTO `personal_access_database`.`available_index` (`index_id`, `position_id`) VALUES ('26', '3');
+INSERT INTO `personal_access_database`.`available_index` (`index_id`, `position_id`) VALUES ('27', '3');
+INSERT INTO `personal_access_database`.`available_index` (`index_id`, `position_id`) VALUES ('28', '3');
+INSERT INTO `personal_access_database`.`available_index` (`index_id`, `position_id`) VALUES ('29', '3');
+INSERT INTO `personal_access_database`.`available_index` (`index_id`, `position_id`) VALUES ('30', '3');
+INSERT INTO `personal_access_database`.`available_index` (`index_id`, `position_id`) VALUES ('31', '3');
+INSERT INTO `personal_access_database`.`available_index` (`index_id`, `position_id`) VALUES ('32', '3');
+INSERT INTO `personal_access_database`.`available_index` (`index_id`, `position_id`) VALUES ('33', '3');
+INSERT INTO `personal_access_database`.`available_index` (`index_id`, `position_id`) VALUES ('34', '3');
+INSERT INTO `personal_access_database`.`available_index` (`index_id`, `position_id`) VALUES ('35', '3');

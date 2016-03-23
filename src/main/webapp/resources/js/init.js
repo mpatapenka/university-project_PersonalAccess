7423e57f908726ecf71f2dc825c32404a06393c3
trollsrulls <maxim131994@gmail.com>
2016-03-23 23:02:36 +0300

    Fixed method for serializing

--- src/main/webapp/resources/js/init.js
+++ src/main/webapp/resources/js/init.js
@@ -7,3 +7,21 @@
 
     $("select").material_select();
 });
+
+// Replacing serializeObject method for serializing forms to Json
+$.fn.serializeObject = function () {
+    var o = {};
+    var a = this.serializeArray();
+
+    $.each(a, function() {
+        if (o[this.name] !== undefined) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+};

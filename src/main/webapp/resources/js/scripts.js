ab5df9f8a6074a230b36682225cf669465d3b31a
trollsrulls <maxim131994@gmail.com>
2016-03-23 22:17:17 +0300

    Generify and optimized js scripts

--- src/main/webapp/resources/js/scripts.js
+++ src/main/webapp/resources/js/scripts.js
@@ -26,6 +26,112 @@ function numberBiggerThenZeroValidator() {
     return isValid;
 }
 
+// Validate estimate fields by name
+function estimateValidator(estimateFieldName) {
+    var isValid = true;
+    $("input[name=" + estimateFieldName + "]").each(function () {
+        $(this).removeClass("invalid");
+        $(this).removeClass("valid");
+
+        var value = $(this).val();
+        if (!jQuery.isNumeric(value)
+            || parseFloat(value) < parseFloat($(this).attr("min"))
+            || parseFloat(value) > parseFloat($(this).attr("max"))) {
+            isValid = false;
+            $(this).addClass("invalid");
+        } else {
+            $(this).addClass("valid");
+        }
+    });
+    return isValid;
+}
+
+
+// Check options of all drop downs, and if they valid
+// reload page with new parameters
+// Store data to event: object with drop downs id's (name 'params'),
+//                      id of form with url for reload (name 'urlId')
+function multipleComboboxChangedReload(event) {
+    var params = event.data["params"];
+    for (var key in params) {
+        if (params.hasOwnProperty(key)) {
+            var value = $(params[key]).val();
+            if (!value) {
+                return;
+            }
+        }
+    }
+
+    var urlFormId = event.data["urlId"];
+    var url = $(urlFormId).attr("action") + "?" + $.param(params);
+    location.replace(url);
+}
+
+// Set up estimates (lead or self) to user indexes
+// Store data to event: field estimate name (name 'fieldName'),
+//                      missing error holder id (name 'missingErrorId')
+//                      unsupported error holder id (name 'unsupportedErrorId')
+//                      form id with send url (name 'sendFormId')
+function setupUserIndexEstimates(event) {
+    var estimateFieldName = event.data["fieldName"];
+    var missingError = event.data["missingErrorId"];
+    var unsupportedError = event.data["unsupportedErrorId"];
+    var sendForm = event.data["sendFormId"];
+
+    if (!emptyValidator()) {
+        Materialize.toast($(missingError).html(), 4000);
+        return;
+    }
+
+    if (!estimateValidator(estimateFieldName)) {
+        Materialize.toast($(unsupportedError).html(), 4000);
+        return;
+    }
+
+    var jsonUserIndexes = collectEstimatesToJSONUserIndexes(estimateFieldName);
+    var form = $(sendForm);
+    $.ajax({
+        url: form.attr("action"),
+        type: form.attr("method"),
+        data: jsonUserIndexes,
+        success: function (result) {
+            if (!result) {
+                console.log("Save user index estimates success.");
+                location.reload();
+            } else {
+                console.log("Server error: " + result);
+                Materialize.toast("Server error, validation not complete!");
+            }
+        },
+        error: function (error) {
+            console.log("Save user index estimates error: " + error);
+            Materialize.toast("Server error when u try to save user index estimates!", 4000);
+        }
+    });
+}
+
+// Collect all estimates field into user index array, which will
+// be converted to JSON
+function collectEstimatesToJSONUserIndexes(estimateFieldName) {
+    var estimates = {};
+    $("input[name=" + estimateFieldName + "]").each(function () {
+        estimates[$(this).attr("id")] = $(this).val();
+    });
+
+    var userIndexes = [];
+    for (var key in estimates) {
+        if (estimates.hasOwnProperty(key)) {
+            var userIndex = {};
+            userIndex["id"] = key;
+            userIndex[estimateFieldName] = estimates[key];
+
+            userIndexes.push(userIndex);
+        }
+    }
+
+    return convertToJsonForTransfer(userIndexes);
+}
+
 
 // Converting JS object to json string with replaced '%' by '%25'
 // It need for transfer by http protocol

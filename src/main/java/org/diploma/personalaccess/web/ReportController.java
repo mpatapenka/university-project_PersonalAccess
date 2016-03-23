2b0d3ca82e7cabd7726b0e37d1cd55ad59c0ed61
trollsrulls <maxim131994@gmail.com>
2016-03-24 01:25:16 +0300

    Created report controller

---
+++ src/main/java/org/diploma/personalaccess/web/ReportController.java
@@ -0,0 +1,14 @@
+package org.diploma.personalaccess.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Report controller, forming pages with reports
+ *
+ * @author Maksim Patapenka
+ */
+@Controller
+@RequestMapping("/report")
+public class ReportController {
+}

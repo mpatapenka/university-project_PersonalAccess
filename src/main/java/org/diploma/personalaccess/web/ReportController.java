5a720ea4b5c89bb8ff7f652028e338155cfd5b04
trollsrulls <maxim131994@gmail.com>
2016-03-24 01:42:37 +0300

    Created pages and mappings for reporting

--- src/main/java/org/diploma/personalaccess/web/ReportController.java
+++ src/main/java/org/diploma/personalaccess/web/ReportController.java
@@ -2,7 +2,9 @@ package org.diploma.personalaccess.web;
 
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Report controller, forming pages with reports
@@ -13,4 +15,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/report")
 public class ReportController {
+
+    @RequestMapping(value = "/employees", method = RequestMethod.GET)
+    public String getReportEmployeesPage(Model model) {
+        return "reportEmployees";
+    }
+
+    @RequestMapping(value = "/indexes", method = RequestMethod.GET)
+    public String getReportIndexesPage(Model model) {
+        return "reportIndexes";
+    }
+
+    @RequestMapping(value = "/ranges", method = RequestMethod.GET)
+    public String getReportRangesPage(Model model) {
+        return "reportRanges";
+    }
+
 }
---
+++ src/main/webapp/jsp/reportEmployees.jsp
@@ -0,0 +1,40 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags" %>
+
+
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
+
+    <title>Report - <spring:message code="global.app_name"/></title>
+
+    <jsp:include page="fragments/static-content.jsp"/>
+</head>
+
+<body>
+    <jsp:include page="fragments/header.jsp"/>
+
+
+    <div class="container">
+        <div class="section">
+
+
+
+        </div>
+    </div>
+
+
+    <jsp:include page="fragments/footer.jsp"/>
+    <jsp:include page="fragments/static-content-js.jsp"/>
+
+    <!-- Custom JS for page -->
+    <script src="<c:url value="/resources/js/pages/report.js"/>"></script>
+</body>
+
+</html>

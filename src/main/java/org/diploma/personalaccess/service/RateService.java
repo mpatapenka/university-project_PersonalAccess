28238cdff9dd5f9965db98d5ebe32c9f8d23718c
trollsrulls <maxim131994@gmail.com>
2016-04-08 23:38:29 +0300

    Updated RateSort type

--- src/main/java/org/diploma/personalaccess/service/RateService.java
+++ src/main/java/org/diploma/personalaccess/service/RateService.java
@@ -31,14 +31,26 @@ public interface RateService {
     enum RateSort {
 
         DOWNWARDS {
+            @Override
             public Comparator<Rate> getComparator() {
                 return (left, right) -> Double.compare(right.getRate(), left.getRate());
+            }
+
+            @Override
+            public long getId() {
+                return 0;
             }
         },
 
         UPWARDS {
+            @Override
             public Comparator<Rate> getComparator() {
                 return (left, right) -> Double.compare(left.getRate(), right.getRate());
+            }
+
+            @Override
+            public long getId() {
+                return 1;
             }
         };
 
@@ -48,6 +60,28 @@ public interface RateService {
          * @return comparator for rate objects
          */
         public abstract Comparator<Rate> getComparator();
+
+        /**
+         * Get id of rate sort object
+         *
+         * @return rate sort object id
+         */
+        public abstract long getId();
+
+        /**
+         * Get rate sort object by id
+         *
+         * @param id specified id
+         * @return rate sort object
+         */
+        public static RateSort getById(long id) {
+            for (RateSort rateSort : values()) {
+                if (rateSort.getId() == id) {
+                    return rateSort;
+                }
+            }
+            return DOWNWARDS;
+        }
 
     }
 

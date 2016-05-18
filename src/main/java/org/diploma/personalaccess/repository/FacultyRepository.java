f02f936ee55f236e1aca6b1a654eb633fe6496fe
trollsrulls <maxim131994@gmail.com>
2016-05-19 01:14:27 +0300

    Added faculty repository

---
+++ src/main/java/org/diploma/personalaccess/repository/FacultyRepository.java
@@ -0,0 +1,9 @@
+package org.diploma.personalaccess.repository;
+
+import org.diploma.personalaccess.entity.Faculty;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FacultyRepository extends JpaRepository<Faculty, Long> {
+}

f678e8c4a0492faf5ecfdd4db886e9fb68312f5c
trollsrulls <maxim131994@gmail.com>
2016-05-19 01:16:58 +0300

    Added js for report indexes

---
+++ src/main/webapp/resources/js/pages/reportIndexes.js
@@ -0,0 +1,15 @@
+;
+var eventParamsForMultipleSelectChanged = {
+    params: {
+        periodId: "#period",
+        year: "#year",
+        posId: "#positions",
+    },
+    urlId: "#sendReloadRequest"
+};
+
+
+// Set listeners to control events
+$("#period").change(eventParamsForMultipleSelectChanged, multipleSelectChangedReload);
+$("#year").change(eventParamsForMultipleSelectChanged, multipleSelectChangedReload);
+$("#positions").change(eventParamsForMultipleSelectChanged, multipleSelectChangedReload);

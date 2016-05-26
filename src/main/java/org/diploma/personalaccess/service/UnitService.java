896e749c7e0aa61d70fd2e91b02d71f5696aac5e
trollsrulls <maxim131994@gmail.com>
2016-05-26 20:56:05 +0300

    Added unit service

---
+++ src/main/java/org/diploma/personalaccess/service/UnitService.java
@@ -0,0 +1,9 @@
+package org.diploma.personalaccess.service;
+
+import org.diploma.personalaccess.entity.Unit;
+
+public interface UnitService {
+
+    Unit getByName(String name);
+
+}
---
+++ src/main/java/org/diploma/personalaccess/service/impl/UnitServiceImpl.java
@@ -0,0 +1,21 @@
+package org.diploma.personalaccess.service.impl;
+
+import org.diploma.personalaccess.entity.Unit;
+import org.diploma.personalaccess.repository.UnitRepository;
+import org.diploma.personalaccess.service.UnitService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UnitServiceImpl implements UnitService {
+
+    @Autowired
+    private UnitRepository unitRepository;
+
+
+    @Override
+    public Unit getByName(String name) {
+        return unitRepository.findByName(name);
+    }
+
+}

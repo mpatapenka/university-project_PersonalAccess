368f6e8064dc57646c4f97702b0bd5871115e4e9
trollsrulls <maxim131994@gmail.com>
2016-05-26 20:56:34 +0300

    Updated LDAP populator

--- src/main/java/org/diploma/personalaccess/service/impl/CustomLdapAuthoritiesPopulator.java
+++ src/main/java/org/diploma/personalaccess/service/impl/CustomLdapAuthoritiesPopulator.java
@@ -1,8 +1,10 @@
 package org.diploma.personalaccess.service.impl;
 
 import org.apache.log4j.Logger;
+import org.diploma.personalaccess.entity.Faculty;
+import org.diploma.personalaccess.entity.Unit;
 import org.diploma.personalaccess.entity.User;
-import org.diploma.personalaccess.service.UserService;
+import org.diploma.personalaccess.service.*;
 import org.diploma.personalaccess.util.ServiceUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ldap.core.DirContextOperations;
@@ -24,13 +26,40 @@ public class CustomLdapAuthoritiesPopulator implements LdapAuthoritiesPopulator 
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private PositionService positionService;
+
+    @Autowired
+    private RoleService roleService;
+
+    @Autowired
+    private FacultyService facultyService;
+
+    @Autowired
+    private UnitService unitService;
+
 
     @Override
     public Collection<? extends GrantedAuthority> getGrantedAuthorities(DirContextOperations dirContextOperations,
                                                                         String username) {
         User user = userService.getUserByUsername(username);
         if (user == null) {
-            user = ServiceUtils.buildUserFromLdapAttributes(dirContextOperations.getAttributes());
+            user = ServiceUtils.buildUserFromLdapAttributes(dirContextOperations.getAttributes(),
+                    positionService.getAll());
+
+            /* Lol I hardcoded it again :) */
+            if ("Проректор".equals(user.getForm().getPosition().getName())) {
+                user.setRole(roleService.getRoleByCode(RoleService.ROLE_ADMIN));
+            } else {
+                user.setRole(roleService.getRoleByCode(RoleService.ROLE_USER));
+            }
+
+            Unit newUnit = unitService.getByName(user.getForm().getUnit().getName());
+            user.getForm().setUnit(newUnit);
+
+            Faculty faculty = ServiceUtils.associateFacultyByUnit(user.getForm().getUnit(), facultyService.getAll());
+            user.getForm().setFaculty(faculty);
+
             userService.addUser(user);
 
             user = userService.getUserByUsername(username);
